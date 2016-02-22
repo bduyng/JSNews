c3b1518cda24157058fbdc0f27bf74c97e458cec
Duy Bao Nguyen <bduyng@gmail.com>
2016-02-22 14:28:03 +0700

    add ArticleListViewModel

---
+++ JSNews/ViewModels/ArticleListViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  ArticleListViewModel.swift
+//  JSNews
+//
+//  Created by Duy Bao Nguyen on 1/25/16.
+//  Copyright © 2016 Duy Bao Nguyen. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class ArticleListViewModel {
+    weak var delegate:ArticleListViewModelDelegate?
+    
+    var articles = [Article]()
+    let pageSize = 30
+    
+    func fetchArticles(sort: String = "top") {
+        Networking.fetchArticles(articles.count / pageSize, sort: sort, completion: {responseArticles in
+            for article in responseArticles {
+                
+                let articleModel = Article()
+                articleModel.title = article.valueForKey("title") as! String
+                articleModel.down = article.valueForKey("down") as! String
+                articleModel.url = article.valueForKey("url") as! String
+                articleModel.up = article.valueForKey("up") as! String
+                articleModel.ctime = article.valueForKey("ctime") as! String
+                articleModel.id = article.valueForKey("id") as! String
+                articleModel.title = article.valueForKey("title") as! String
+                articleModel.username = article.valueForKey("username") as! String
+                articleModel.comments = article.valueForKey("comments") as! String
+                
+                self.articles.append(articleModel)
+                
+                self.delegate?.didFetchedArticles()
+            }
+        })
+    }
+}
+
+protocol ArticleListViewModelDelegate: class {
+    func didFetchedArticles()
+}
