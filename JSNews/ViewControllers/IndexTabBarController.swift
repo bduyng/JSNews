6d3690b8bcc92767040c9614c4fd5d7de18952cd
Duy Bao Nguyen <bduyng@gmail.com>
2016-03-05 11:59:17 +0700

    add comments in IndexTabBarController

--- JSNews/ViewControllers/IndexTabBarController.swift
+++ JSNews/ViewControllers/IndexTabBarController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class IndexTabBarController: UITabBarController {
 
+    // MARK: - View Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,7 +18,7 @@ class IndexTabBarController: UITabBarController {
         self.tabBar.tintColor = UIColor.primaryColor()
         
         // select middle tab as default
-        self.selectedIndex = 1
+        self.selectedIndex = 0
 
         // Do any additional setup after loading the view.
     }
@@ -27,17 +28,7 @@ class IndexTabBarController: UITabBarController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
+    // MARK: - UITabBarDelegate
     override func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
         if self.selectedIndex == 1 && tabBar.items![self.selectedIndex] == item {
             item.title = item.title == "Top" ? "Latest" : "Top"
