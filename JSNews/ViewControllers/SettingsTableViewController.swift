a63bceeb5ec007ad3de7c9ee9607a8a6bfb2f026
Duy Bao Nguyen <bduyng@gmail.com>
2016-03-07 16:47:24 +0700

    update settings page

--- JSNews/ViewControllers/SettingsTableViewController.swift
+++ JSNews/ViewControllers/SettingsTableViewController.swift
@@ -7,6 +7,13 @@
 //
 
 import UIKit
+import SafariServices
+
+struct SettingsConstants {
+    static let Twitter = 0
+    static let Github = 1
+    static let Email = 2
+}
 
 class SettingsTableViewController: UITableViewController {
 
@@ -23,13 +30,44 @@ class SettingsTableViewController: UITableViewController {
     
     // MARK: - UITableViewDelegate
     
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        if (indexPath.section == 0) {
+            cell.selectionStyle = .None
+        }
+    }
+    
     override func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         (view as! UITableViewHeaderFooterView).textLabel!.textColor = UIColor.primaryColor()
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        print(indexPath.section)
-        print(indexPath.row)
+        if (indexPath.section == 0) {
+            for cell in tableView.visibleCells {
+                if cell.accessoryType == .Checkmark {
+                    cell.accessoryType = .None
+                }
+            }
+            
+            let selectedCell = tableView.cellForRowAtIndexPath(indexPath)
+            selectedCell?.accessoryType = .Checkmark
+        }
+        else if (indexPath.section == 1) {
+            var url : String?
+            switch indexPath.row {
+                case SettingsConstants.Twitter:
+                    url = "https://twitter.com/bduyng"
+                case SettingsConstants.Github:
+                    url = "https://github.com/bduyng"
+                default:
+                    url = nil
+            }
+            
+            if (url != nil) {
+                let safariVC = SFSafariViewController(URL: NSURL(string: url!)!, entersReaderIfAvailable: true)
+                safariVC.view.tintColor = UIColor.primaryColor()
+                self.presentViewController(safariVC, animated: true, completion: nil)
+            }
+        }
         
     }
 }
