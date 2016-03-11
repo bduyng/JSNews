43716a1ba51949fd76e318c6b3f810fb14ae3166
Duy Bao Nguyen <bduyng@gmail.com>
2016-03-11 09:14:58 +0700

    update ArticlePresenter

--- JSNews/Supports/ArticlePresenter.swift
+++ JSNews/Supports/ArticlePresenter.swift
@@ -6,53 +6,29 @@
 //  Copyright © 2016 Duy Bao Nguyen. All rights reserved.
 //
 
-import Foundation
+//import Foundation
 import UIKit
+import SafariServices
 
-protocol ArticlePresenter: WKWebViewControllerDelegate {
+protocol ArticlePresenter {
     func openArticle(article: Article, tableView: UITableView, indexPath: NSIndexPath)
-}
-
-extension ArticlePresenter {
-    func didDismissWebView(tableView: UITableView, indexPath: NSIndexPath) {
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
-    }
 }
 
 extension ArticlePresenter where Self: UIViewController {
     func openArticle(article: Article, tableView: UITableView, indexPath: NSIndexPath) {
-        if let rangeOfHttpStr = String(article.url).rangeOfString("http") where rangeOfHttpStr.startIndex == String(article.url).startIndex {
-            // open the article by wkwebview
-            let webViewNavVC = storyboard?.instantiateViewControllerWithIdentifier("WebViewNavigationController") as! UINavigationController
+        if article.url.indexOf("http") == 0 {
             
-            let webViewVC = webViewNavVC.viewControllers.first as! WKWebViewController
-            webViewVC.article = article
-            webViewVC.tableView = tableView
-            webViewVC.indexPath = indexPath
-            webViewVC.delegate = self
+            let defaults = NSUserDefaults.standardUserDefaults()
             
-            webViewNavVC.transitioningDelegate = self
-            webViewNavVC.modalPresentationStyle = .Custom
-            self.presentViewController(webViewNavVC, animated: true, completion: nil)
-            
-            article.saveArticleIntoHistoryList()
+            let safariVC = SFSafariViewController(URL: NSURL(string: article.url)!, entersReaderIfAvailable: defaults.boolForKey("EnterReaderModeFirst"))
+            safariVC.view.tintColor = UIColor.primaryColor()
+            self.presentViewController(safariVC, animated: true, completion: {
+                article.saveArticleIntoHistoryList()
+            })
         }
         else {
             print("Error")
             print(article.url)
         }
-    }
-}
-
-extension UIViewController: UIViewControllerTransitioningDelegate {
-    public func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        
-        let presentationAnimator = TransitionPresentationAnimator()
-        return presentationAnimator
-    }
-    
-    public func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        let dismissalAnimator = TransitionDismissalAnimator()
-        return dismissalAnimator
     }
 }
